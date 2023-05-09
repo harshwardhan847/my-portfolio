2e45720d0b089a8b8a271966099083caf9e12758
Harsh <harshwardhan847@gmail.com>
2023-05-09 09:52:19 +0530

    made responsive for mobile

--- src/components/intro/navigation-bar/index.jsx
+++ src/components/intro/navigation-bar/index.jsx
@@ -1,36 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
+import { CgMenuRight } from "react-icons/cg";
+import { IoMdClose } from "react-icons/io";
 import logo from "../../../images/logo.png";
 import CallToAction from "../../shared/CallToAction";
 import scrollToSection from "../../utils/helper.js";
 const Navigation = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const menuItemClickHandler = (section) => {
+    setMobileMenu(!mobileMenu);
+    scrollToSection(section);
+  };
   return (
     <nav className="top-navigation-bar">
       <div className="app-logo">
         <img src={logo} alt="Harsh Wardhan" />
       </div>
-      <div className="navigation">
+      <div className="mobile-menu" onClick={() => setMobileMenu(!mobileMenu)}>
+        {mobileMenu ? (
+          <IoMdClose size={24} color="fff" />
+        ) : (
+          <CgMenuRight size={24} color="fff" />
+        )}
+      </div>
+      <div className={`navigation ${mobileMenu ? "active" : ""}`}>
         <span
           className="navigation-item"
-          onClick={() => scrollToSection("skills")}
+          onClick={() => menuItemClickHandler("skills")}
         >
           Skills
         </span>
         <span
           className="navigation-item"
-          onClick={() => scrollToSection("portfolio")}
+          onClick={() => menuItemClickHandler("portfolio")}
         >
           Portfolio
         </span>
         <span
           className="navigation-item"
-          onClick={() => scrollToSection("blogs")}
+          onClick={() => menuItemClickHandler("blogs")}
         >
           Blogs & Articles
         </span>
         <CallToAction
           text="Contact Me"
-          action={() => scrollToSection("contact")}
+          action={() => menuItemClickHandler("contact")}
         />
       </div>
     </nav>
